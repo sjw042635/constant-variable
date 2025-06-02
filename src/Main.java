0daa8d0e21e82b648f33c94144ea758abd4166d7
sjw042635 <jwsung10@naver.com>
2025-06-02 10:12:40 +0900

    constant-variable

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import org.w3c.dom.ls.LSOutput;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello, World!");
+    final int DAYS_IN_WEEK = 10;
+
   }
 }
