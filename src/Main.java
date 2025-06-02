fde6ce2b515810858207e647f61af08d21392dbc
sjw042635 <jwsung10@naver.com>
2025-06-02 10:36:56 +0900

    Four-basic-operations

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,18 @@ import org.w3c.dom.ls.LSOutput;
 
 public class Main {
   public static void main(String[] args) {
-    final int DAYS_IN_WEEK = 10;
+    int a = 10 , b = 3;
 
+    int sum = a + b;
+
+    System.out.println(sum);
+    int difference = a - b;
+    System.out.println(difference);
+    int product = a * b;
+    System.out.println(product);
+    int quotient = a / b;
+    System.out.println(quotient);
+    int remainder = a % b;
+    System.out.println(remainder);
   }
 }
